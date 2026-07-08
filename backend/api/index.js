8c20641766cb89a5dbc7a7479572c17efe5ff93c
tata727105-rgb <ta.ta727105@gmail.com>
2026-07-08 12:35:04 +0100

    Simplify Vercel configuration

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,6 @@
 const app = require('../src/app')
 
 // Export the Express app for Vercel serverless deployment
-// Vercel will automatically handle the Express app
-module.exports = app
+module.exports = (req, res) => {
+  app(req, res)
+}
